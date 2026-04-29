91d192d26c31871a13aee268b9069511a69a8d0b
Blaiso <blaiso472eyor@gmail.com>
2026-04-29 12:56:52 +0100

    mise a jour de cle voiture

--- static/js/defilement.js
+++ static/js/defilement.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 // On récupère la position de la section par rapport au haut de la page
                 const elementPosition = targetSection.getBoundingClientRect().top;
                 // On définit la marge (hauteur de ton header + un peu d'espace)
-                const offset = 100; 
+                const offset = 50; 
                 // On calcule la position finale
                 const offsetPosition = elementPosition + window.pageYOffset - offset;
                 // On utilise window.scrollTo au lieu de scrollIntoView pour plus de précision
